34495313e00aee7dbbe120cd21a2d9823e869915
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2023-09-22 06:47:05 +0000

    test

--- R/zzz.R
+++ R/zzz.R
@@ -23,35 +23,26 @@ Load_Python_Scripts <- function(){
   
   
   cat("\nLoading Python env: ...")
-  switch(get_os(),
-         windows = {
-           if (!reticulate::virtualenv_exists('r-Pirat')){
-              warning("No 'r-Pirat' virtualenv exists. 
-                    You should install one first by running: install_Pirat()")
-                return()
-             } else {
-               reticulate::use_virtualenv("r-Pirat")
-             }
-           },
-           linux = {
-             if(!('r-Pirat' %in% reticulate::conda_list()$name)){
-                warning("No conda 'r-Pirat' env exists. 
-                You should install one first by running: install_Pirat()")
-                return()
-            } else {
-              reticulate::use_condaenv("r-Pirat")
-            }
-             },
-         default = {
-           warning("OS not detected or unmanaged")
-           return()
-         }
-  )
+  pirat_conda_exists <- 'r-pirat' %in% reticulate::conda_list()$name
+  pirat_venv_exists <-reticulate::virtualenv_exists('r-pirat')
+    
+  
+  if (!pirat_conda_exists && !rpirat_venv_exists){
+  warning("No 'r-pirat' env exists. 
+          You should install one first by running: install_Pirat()")
+    return()
+    } 
+  
+  if (pirat_conda_exists)
+    reticulate::use_condaenv("r-pirat")
+  else if (pirat_venv_exists)
+    reticulate::use_virtualenv("r-pirat")
+
   cat("done")
 
   
   # Now, install custom Python scripts
-  cat("\nInstalling custom Python scripts: ...")
+  cat("\nSourcing custom Python scripts: ...")
   dir.backup <- getwd()
   setwd(system.file(".", package="Pirat"))
   reticulate::source_python(system.file("python", "LBFGS.py", package = "Pirat"))
@@ -66,6 +57,15 @@ Load_Python_Scripts <- function(){
 
 
 
+#' @export
+Get_active_env <- function(){
+  
+  py_home <- reticulate::py_config()$pythonhome
+  tmp <- strsplit(py_home, split="/")[[1]]
+  active_env <- tmp[length(tmp)]
+  
+  active_env
+}
 
 #' Pirat configuration information
 #'
@@ -76,53 +76,90 @@ Load_Python_Scripts <- function(){
 #'
 #' @keywords internal
 #' @export
-torch_config <- function() {
+pirat_config <- function() {
   
-  # first check if we found tensorflow
-  have_Pirat<- py_module_available("torch")
+  # first check if we found pytorch
+  have_torch <- reticulate::py_module_available("torch")
   
   # get py config
-  config <- py_config()
+  config <- reticulate::py_config()
   
   # found it!
-  if (have_tensorflow) {
+  if (have_torch) {
     
     # get version
-    if (reticulate::py_has_attr(tf, "version"))
-      version_raw <- tf$version$VERSION
-    else
-      version_raw <- tf$VERSION
+    ind <- which(reticulate::py_list_packages()$package=='pytorch')
+    torch_version <- reticulate::py_list_packages()[ind,]$version
     
-    tfv <- strsplit(version_raw, ".", fixed = TRUE)[[1]]
-    version <- package_version(paste(tfv[[1]], tfv[[2]], sep = "."))
+    ind <- which(reticulate::py_list_packages()$package=='numpy')
+    numpy_version <- reticulate::py_list_packages()[ind,]$version
     
-    structure(class = "tensorflow_config", list(
+    ind <- which(reticulate::py_list_packages()$package=='matplotlib')
+    matplotlib_version <- reticulate::py_list_packages()[ind,]$version
+    
+    
+    structure(class = "pirat_config", list(
       available = TRUE,
-      version = version,
-      version_str = version_raw,
-      location = config$required_module_path,
+      acive_env = Get_active_env(),
+      torch_version = torch_version,
+      numpy_version = numpy_version,
+      matplotlib_version = matplotlib_version,
       python = config$python,
-      python_version = config$version
+      python_version = reticulate::py_version()
     ))
     
     # didn't find it
   } else {
-    structure(class = "tensorflow_config", list(
+    structure(class = "pirat_config", list(
       available = FALSE,
       python_versions = config$python_versions,
-      error_message = tf_config_error_message()
+      #error_message = pirat_config_error_message()
+      error_message = NULL
     ))
   }
 }
 
 
-#' @rdname tf_config
+#' @rdname pirat_config
 #' @keywords internal
 #' @export
 torch_version <- function() {
-  config <- torch_config()
+  config <- pirat_config()
   if (config$available)
-    config$version
+    config$torch_version
   else
     NULL
+}
+
+
+# Build error message for TensorFlow configuration errors
+pirat_config_error_message <- function() {
+  message <- "Valid installation of Pirat not found."
+  config <- pirat_config()
+  if (!is.null(config)) {
+    if (length(config$python_versions) > 0) {
+      message <- paste0(message,
+                        "\n\nPython environments searched for 'tensorflow' package:\n")
+      python_versions <- paste0(" ", normalizePath(config$python_versions, mustWork = FALSE),
+                                collapse = "\n")
+      message <- paste0(message, python_versions, sep = "\n")
+    }
+  }
+  
+  python_error <- tryCatch({
+    import("tensorflow")
+    list(message = NULL)
+  },
+  error = function(e) {
+    on.exit(py_clear_last_error())
+    py_last_error()
+  })
+  
+  message <- paste0(message,
+                    "\nPython exception encountered:\n ",
+                    python_error$message, "\n")
+  
+  message <- paste0(message,
+                    "\nYou can install TensorFlow using the install_tensorflow() function.\n")
+  message
 }
