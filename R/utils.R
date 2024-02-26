e0ddf09ba42c61ccc208c4ca125734a9d2a804f1
Lucas Etourneau <lucas.etourneau1@outlook.fr>
2024-02-26 10:03:38 +0100

    Add docs about seed fixed

--- R/utils.R
+++ R/utils.R
@@ -226,9 +226,11 @@ rm_pg_from_idx_merge_pg <- function(l_pep_rna, pg_idx) {
 
 #' @title Split too large PGs
 #' 
-#' @description Split PGs with too many peptides/precursors. It creates 
-#' different PGs with size equal to size max. Hence, some peptides can be
-#'  duplicated in the new PGs.
+#' @description Split PGs with too many peptides/precursors, while keeping other PG untouched. It creates 
+#' new PGs with size equal to size max. Hence, some peptides can be
+#'  duplicated in the new PGscreated. The seed is fixed such that the split generated remains constant for a given
+#'  adjacency matrix. 
+#'  
 #'
 #' @param adj Adjacency matrix between peptides and PGs.
 #' @param size_max Maximum PG size desired.
@@ -403,12 +405,14 @@ impute_block_llk_reset = function(data.pep.rna.crop,
 }
 
 # TODO: Rename the function
-#' @title Split too large PGs in multi-omic context
+#' @title Splits too large PGs in proteogenomics context
 #' 
-#' @description Split PGs with too many peptides/precursors, and adapts 
-#' adjacency matrix between mRNA and PGs accordingly. It creates different PGs 
+#' @description Split PGs with too many peptides/precursors while keeping other PGs untouched,
+#' and adapts adjacency matrix between mRNA and PGs accordingly. It creates new PGs 
 #' with size equal to size max (including peptides and mRNAs). Hence, some 
 #' peptides and mRNA can be duplicated in the new PGs.
+#' The seed is fixed such that the split generated remains constant for a given
+#' adjacency matrix. 
 #'
 #' @param adj Adjacency matrix between peptides and PGs.
 #' @param size_max Maximum PG size desired.
