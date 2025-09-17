dce8fe3742d3ca1262b926989cee8c7f77c3b683
Lucas Etourneau <lucas.etourneau@berkeley.edu>
2025-09-16 17:23:29 -0700

    Version cpuonly

--- R/basilisk.R
+++ R/basilisk.R
@@ -6,7 +6,7 @@
 #' 
 envPirat <- basilisk::BasiliskEnvironment("envPirat",
     pkgname = "Pirat",
-    packages = c("python==3.9.16", "numpy==1.20.2", "cpuonly==2.0", "pytorch==1.12.0"),
+    packages = c("python==3.9.16", "numpy==1.20.2", "cpuonly==1.0", "pytorch==1.12.0"),
     #channels = c("conda-forge", "pytorch", "torch", "nodefaults"),
     channels = c("conda-forge", "pytorch", "main", "defaults"),
     path = "myModules"
