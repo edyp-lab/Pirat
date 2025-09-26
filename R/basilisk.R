adfcaca8480877d0b4bfcb15cc46a1e327948105
Lucas Etourneau <lucas.etourneau@berkeley.edu>
2025-09-26 16:26:56 -0700

    Change python numpy

--- R/basilisk.R
+++ R/basilisk.R
@@ -6,7 +6,7 @@
 #' 
 envPirat <- basilisk::BasiliskEnvironment("envPirat",
     pkgname = "Pirat",
-    packages = c("python==3.11", "numpy==1.20.2", "pytorch==1.12.0", "cpuonly==2.0"),
+    packages = c("python==3.10", "numpy==1.21", "pytorch==1.12.0", "cpuonly==2.0"),
     #channels = c("conda-forge", "pytorch", "torch", "nodefaults"),
     channels = c("conda-forge", "pytorch"),
     path = "myModules"
