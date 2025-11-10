b2fe8bb6469c8072b647d376d35dadf14ac30db0
Lucas Etourneau <lucas.etourneau@berkeley.edu>
2025-11-10 12:12:07 +0100

    Add +cpu

--- R/basilisk.R
+++ R/basilisk.R
@@ -7,6 +7,6 @@
 envPirat <- basilisk::BasiliskEnvironment(
   "envPirat",
   pkgname = "Pirat",
-  packages = c("python=3.10", "torch=1.13.1", "numpy=1.21"), 
+  packages = c("python=3.10", "torch=1.13.1+cpu", "numpy=1.21"), 
   path = "myModules"
 )
