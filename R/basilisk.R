9e517e49000400cf83576264f986af1a74802c8b
Lucas Etourneau <lucas.etourneau@berkeley.edu>
2025-11-12 16:51:34 +0100

    Correct last commit

--- R/basilisk.R
+++ R/basilisk.R
@@ -5,7 +5,7 @@
 #' @return An instance of the class `BasiliskEnvironment`
 #' 
 # If machine is Linux or Windows, add +cpu to torch version
-torch_version <- if (.Platform$OS.type == "unix") {
+torch_version <- if (Sys.info()["sysname"] == "Darwin") {
   "torch==1.13.1"
 } else {
   "torch==1.13.1+cpu"
