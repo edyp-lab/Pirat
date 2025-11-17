c962d20af3cdd69b2b2ebc59355b120c8aabedbf
Lucas Etourneau <lucas.etourneau@berkeley.edu>
2025-11-18 00:44:55 +0100

    Change versions

--- R/basilisk.R
+++ R/basilisk.R
@@ -5,14 +5,14 @@
 #' @return An instance of the class `BasiliskEnvironment`
 #' 
 # If machine is Linux or Windows, add +cpu to torch version
-torch_version <- if (Sys.info()["sysname"] == "Darwin") {
-  "torch==1.13.1"
-} else {
-  "torch==1.13.1+cpu"
-}
+# torch_version <- if (Sys.info()["sysname"] == "Darwin") {
+#   "torch==1.13.1"
+# } else {
+#   "torch==1.13.1"
+# }
 envPirat <- basilisk::BasiliskEnvironment(
   "envPirat",
   pkgname = "Pirat",
-  packages = c("python=3.10", torch_version, "numpy=1.21"), 
+  packages = c("python=3.11", "torch=2.5.1", "numpy=1.24"), 
   path = "myModules"
 )
